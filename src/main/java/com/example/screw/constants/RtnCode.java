9339750faac1238c7b87f520f35c8e26d322e4f2
Yuu_quoi <hjing378@gmail.com>
2024-05-15 11:09:08 +0800

    小修

--- src/main/java/com/example/screw/constants/RtnCode.java
+++ src/main/java/com/example/screw/constants/RtnCode.java
@@ -3,16 +3,14 @@ package com.example.screw.constants;
 public enum RtnCode {
 
 
-	ORDER_NO_NOT_EXISTS(400, "Not exists order no !!"), //
-	ORDER_NO_DUPLICATED(400, "Duplicated order no !!"), //
 	VOLTAGE_CANNOT_BE_ZERO(400, "Voltage can not be zero"),//
 	MACHINE_NAME_CANNOT_BE_NULL(400, "Machine name can not be null"),
-	PASSWORD_ERROR(400, "Password error !!"), //
-	ACCOUNT_NOT_EXISTS(400, "Account not exists !!"), //
+	PASSWORD_ERROR(400, "密碼錯誤"), //
+	ACCOUNT_NOT_EXISTS(400, "帳號不存在"), //
 	ACCOUNT_EXISTS(400, "Account already exists !!"), //
 	VOLTAGE_CANNOT_ZERO(400, "Voltage_Cannot_Zero"),//
 	ORDER_NUMBER_NOT_EXISTS(400, "Not exists order numner !!"), //
-	ORDER_NUMBER_DUPLICATED(400, "Duplicated order number !!"), //
+	ORDER_NUMBER_DUPLICATED(400, "單號已存在"), //
 	JSON_ERROR(400, "JSON error!!"), //
 	MACHINE_NAME_NOT_FOUND(404, "Machine name not found"),//
 	PLEASE_LOGIN_FIRST(404, "Please login first"), //
